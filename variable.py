56c58c3b6d5c30df065a4d2bb3482bb585dd01d0
zyrell <raniasabirhussain@gmail.com>
2025-12-24 23:05:45 +0800

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,11 @@
+# Generic Info Program
+
+# Store information about a fictional character
+city = "Metropolis"
+favorite_color = "Blue"
+lucky_number = 42
+
+# Print all information in a single sentence
+print( "They live in", city, 
+      "their favorite color is", favorite_color, 
+     "and their lucky number is", lucky_number)
